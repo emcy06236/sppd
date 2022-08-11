1fc09c9698e8191440215b79ae1198d567cd0b95
emcy06236 <107631003+emcy06236@users.noreply.github.com>
2022-08-12 00:25:15 +0100

    added youtube search thingy

--- sppd.py
+++ sppd.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-# TODO: find way to download youtube vid; argv checking; link checking; fancy output
+# TODO: find way to download youtube vid (50% compl); argv checking; link checking; fancy output
+import sys
 import re
-from bs4 import BeautifulSoup
 import urllib.request
 from urllib.error import URLError
-import sys
+from bs4 import BeautifulSoup
+from pytube import Search
 
+namelist = []
+ytlist = []
 playlist = sys.argv[1]
 
 try:
@@ -19,6 +22,25 @@ except URLError as e:
 
 soup = BeautifulSoup(html_doc, 'html.parser')
 
+# Get song names
+
 for link in soup.find_all('button'):
     if re.search("track", str(link.get('aria-label'))):
-        print(link.get('aria-label'))
+        trackname = link.get('aria-label')
+        name = trackname.removeprefix('track ')
+        namelist.append(name)
+
+# Get album name
+rawalbumname = soup.find_all(attrs={"property" : "og:title"})
+albumname = rawalbumname[0]['content']
+
+# Put 2 and 2 together and search youtube
+for ogname in namelist:
+    #print(albumname + ' ' + ogname + ' lyrics')
+    s = Search(albumname + ' ' + ogname + ' lyrics')
+    ytlink = s.results[0]
+    print(ytlink.watch_url)
+    ytlist.append(ytlink.watch_url)
+
+#print(ytlist)
+

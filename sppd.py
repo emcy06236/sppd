79eff91fc4b725daaac47973c2d7396dbf0d6a81
emcy06236 <107631003+emcy06236@users.noreply.github.com>
2022-08-12 12:47:07 +0100

    make it download the music

--- sppd.py
+++ sppd.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
-# TODO: find way to download youtube vid (50% compl); argv checking; link checking; fancy output
+# TODO: argv checking; link checking; fancy output; multiple downloads at same time
 import sys
+import os
 import re
 import urllib.request
+from os.path import expanduser
 from urllib.error import URLError
 from bs4 import BeautifulSoup
 from pytube import Search
+from pytube import YouTube
+
+if os.name == 'posix':
+    downloadloc = os.environ.get('XDG_MUSIC_DIR', '~/Music/')
+elif os.name == 'nt':
+    downloadloc = expanduser("~\\Music")
+
+
 
 namelist = []
 ytlist = []
@@ -28,7 +38,9 @@ for link in soup.find_all('button'):
     if re.search("track", str(link.get('aria-label'))):
         trackname = link.get('aria-label')
         name = trackname.removeprefix('track ')
-        namelist.append(name)
+        clean_name = re.sub('[^\w_.)( -]', '', name)
+        print(clean_name)
+        namelist.append(clean_name)
 
 # Get album name
 rawalbumname = soup.find_all(attrs={"property" : "og:title"})
@@ -36,11 +48,18 @@ albumname = rawalbumname[0]['content']
 
 # Put 2 and 2 together and search youtube
 for ogname in namelist:
-    #print(albumname + ' ' + ogname + ' lyrics')
-    s = Search(albumname + ' ' + ogname + ' lyrics')
+    s = Search(albumname + ' ' + ogname)
     ytlink = s.results[0]
     print(ytlink.watch_url)
     ytlist.append(ytlink.watch_url)
 
-#print(ytlist)
+# Download
+for yl, nl in zip(ytlist, namelist):
+    yt = YouTube(yl)
+    nlfile = nl + '.webm'
+    stream = yt.streams.get_by_itag(251)
+    streamsize = stream.filesize / 1000000
+    for_streamsize = "{:.2f}".format(streamsize)
 
+    print('Downloading ' + '"' + nl + '"' + ' (' + str(for_streamsize) + 'MB)' + ' to ' + downloadloc)
+    stream.download(output_path=downloadloc, filename=nlfile)
